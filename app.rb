b0f5ec4144f5493a5f5611f312bcfb1de11deb05
Leigh-ann <leighann.gant@gmail.com>
2018-01-22 21:27:56 +0000

    First user story implemented

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra/base'
+require './models/link.rb'
 
 class BookMarkManager < Sinatra::Base
   get '/' do
     'Hello!'
   end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
 end
