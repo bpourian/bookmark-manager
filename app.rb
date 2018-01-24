e252d574328e4a34ca868ff292e24aef2c909a3b
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-01-24 22:33:13 +0000

    feature test written for signup page

--- app.rb
+++ app.rb
@@ -13,20 +13,31 @@ class BookMarkManager < Sinatra::Base
     erb :'links/index'
   end
 
-  get '/links/new' do
-    erb :'links/add_link'
-  end
-
   post '/links' do
     link = Link.first_or_create(url:params[:url],title:params[:title])
     Tag.multiple_tags(params[:tags], link)
     redirect '/links'
   end
 
+  get '/links/new' do
+    erb :'links/add_link'
+  end
+
   get '/tags/:name' do
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb :'links/index'
+  end
+
+  post '/signup' do
+    p params
+    params[:email]
+    params[:password]
+    redirect '/links'
+  end
+
+  get '/signup' do
+    erb :'register/signup'
   end
 
 end
