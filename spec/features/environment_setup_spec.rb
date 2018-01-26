189403f92dc637ce295b1861d145542b2729ef00
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-01-26 10:42:03 +0000

    tests green with ne implemented changes made to layout page

--- spec/features/environment_setup_spec.rb
+++ spec/features/environment_setup_spec.rb
@@ -1,6 +1,6 @@
 feature 'environment set up correctly' do
   scenario 'set up' do
     visit('/')
-    expect(page).to have_content 'Welcome Bookmark Manager'
+    expect(page).to have_content 'Welcome to Bookmark Manager'
   end
 end
